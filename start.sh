a80cbc5b152905770c0fc8a8c4ab09efc4d07610
rebecca554owen <rebecca554owen@gmail.com>
2024-04-15 00:30:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# 更新系统和安装必需的工具
+echo "更新系统和安装依赖..."
+apt update -y && apt install -y sudo screen unzip wget
+
+# 创建目录并进入
+mkdir -p /etc/ppp
+cd /etc/ppp
+
 # 定义安装和管理PPP的函数
 function install_ppp() {
     echo "更新系统和安装依赖..."
@@ -45,10 +53,6 @@ function install_ppp() {
     echo "解压下载的文件..."
     unzip -o '*.zip' && rm *.zip
     chmod +x ppp
-
-    echo "下载配置文件..."
-    wget -O appsettings.json https://raw.githubusercontent.com/rebecca554owen/openppp2-docker/main/appsettings.json
-    echo "请手动编辑配置文件以匹配您的设置：vim appsettings.json"
 
     echo "配置系统服务..."
     cat > /etc/systemd/system/ppp.service << EOF
@@ -101,9 +105,178 @@ function view_ppp_session() {
     screen -r ppp
 }
 
+function create_or_modify_config() {
+    echo "配置或修改PPP配置文件..."
+    ppp_config="/etc/ppp/appsettings.json"
+    if [[ -f "$ppp_config" ]]; then
+        echo "已存在配置文件：$ppp_config"
+        echo "是否要编辑现有的配置文件？ (y/n)"
+        read edit_choice
+        if [[ "$edit_choice" == "y" ]]; then
+            vim "$ppp_config"
+            echo "配置文件已修改。"
+            restart_ppp
+        else
+            echo "跳过修改。"
+            retun
+        fi
+    else
+    # 如果配置文件不存在，则重新生成配置文件
+    echo -e "${yellow}重新生成${ppp_config}。${plain}"
+    # 检测公网出口/内网IP来提示用户
+    curl -m 10 -s ip.sb
+    ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1
+    
+    read -p "请输入VPS IP: " vps_ip
+    read -p "请输入VPS 端口: " port
+    # 设置监听Interface的默认值::用于ipv6
+    default_lan_ip="::"
+    read -p "请输入内网IP地址（默认为${default_lan_ip}，服务端保持默认值即可）: " lan_ip
+    lan_ip=${lan_ip:-$default_lan_ip}
+    # 设置线程数，随机uuid，避免多客户端时候冲突。
+    concurrent=$(nproc)
+    random_guid=$(uuidgen)
+    
+    echo -e "${yellow} 节点 ${vps_ip}:${port} 线程数 ${concurrent} 用户ID ${random_guid} ${plain}"
+    
+    cat >"${ppp_config}" <<EOF
+{
+    "concurrent": ${concurrent},
+    "cdn": [2080, 2443],
+    "key": {
+        "kf": 154543927,
+        "kx": 128,
+        "kl": 10,
+        "kh": 12,
+        "protocol": "aes-128-cfb",
+        "protocol-key": "N6HMzdUs7IUnYHwq",
+        "transport": "aes-256-cfb",
+        "transport-key": "HWFweXu2g5RVMEpy",
+        "masked": false,
+        "plaintext": false,
+        "delta-encode": false,
+        "shuffle-data": false
+    },
+    "ip": {
+        "public": "${vps_ip}",
+        "interface": "${lan_ip}"
+    },
+    "vmem": {
+        "size": 0,
+        "path": "./"
+    },
+    "tcp": {
+        "inactive": {
+            "timeout": 300
+        },
+        "connect": {
+            "timeout": 5
+        },
+        "listen": {
+            "port": ${port}
+        },
+        "turbo": true,
+        "backlog": 511,
+        "fast-open": true
+    },
+    "udp": {
+        "inactive": {
+            "timeout": 72
+        },
+        "dns": {
+            "timeout": 4,
+            "redirect": "0.0.0.0"
+        },
+        "listen": {
+            "port": ${port}
+        },
+        "static": {
+            "keep-alive": [1, 5],
+            "dns": true,
+            "quic": true,
+            "icmp": true,
+            "servers": ["${vps_ip}:${port}"]
+        }
+    },
+    "websocket": {
+        "host": "starrylink.net",
+        "path": "/tun",
+        "listen": {
+            "ws": 20080,
+            "wss": 20443
+        },
+        "ssl": {
+            "certificate-file": "starrylink.net.pem",
+            "certificate-chain-file": "starrylink.net.pem",
+            "certificate-key-file": "starrylink.net.key",
+            "certificate-key-password": "test",
+            "ciphersuites": "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256"
+        },
+        "verify-peer": true,
+        "http": {
+            "error": "Status Code: 404; Not Found",
+            "request": {
+                "Cache-Control": "no-cache",
+                "Pragma": "no-cache",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "zh-CN,zh;q=0.9",
+                "Origin": "http://www.websocket-test.com",
+                "Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+            },
+            "response": {
+                "Server": "Kestrel"
+            }
+        }
+    },
+    "server": {
+        "log": "./ppp.log",
+        "node": 1,
+        "subnet": true,
+        "mapping": true,
+        "backend": "",
+        "backend-key": "HaEkTB55VcHovKtUPHmU9zn0NjFmC6tff"
+    },
+    "client": {
+        "guid": "{${random_guid}}",
+        "server": "ppp://${vps_ip}:${port}/",
+        "bandwidth": 0,
+        "reconnections": {
+            "timeout": 5
+        },
+        "paper-airplane": {
+            "tcp": true
+        },
+        "http-proxy": {
+            "bind": "${lan_ip}",
+            "port": 8080
+        },
+        "mappings": [
+            {
+                "local-ip": "${lan_ip}",
+                "local-port": 80,
+                "protocol": "tcp",
+                "remote-ip": "::",
+                "remote-port": 10001
+            },
+            {
+                "local-ip": "${lan_ip}",
+                "local-port": 7000,
+                "protocol": "udp",
+                "remote-ip": "::",
+                "remote-port": 10002
+            }
+        ]
+    }
+}
+EOF
+        echo "新配置文件已创建。"
+    fi
+}
+
 # 显示菜单并处理用户输入
 PS3='请选择一个操作: '
-options=("安装PPP" "启动PPP" "停止PPP" "重启PPP" "更新PPP" "查看PPP会话" "退出")
+options=("安装PPP" "启动PPP" "停止PPP" "重启PPP" "更新PPP" "查看PPP会话" "配置或修改配置文件" "退出")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -124,6 +297,9 @@ do
             ;;
         "查看PPP会话")
             view_ppp_session
+            ;;
+        "配置或修改配置文件")
+            create_or_modify_config
             ;;
         "退出")
             break

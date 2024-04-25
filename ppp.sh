52f1dd32488248dddee049d80f4a58b9ff0aaa3b
rebecca554owen <rebecca554owen@gmail.com>
2024-04-25 13:07:48 +0800

    Update ppp.sh

--- ppp.sh
+++ ppp.sh
@@ -268,7 +268,7 @@ services:
       - "8080:8080"
     networks:
       - ${ppp_name}network
-    command: ppp --mode=client --tun-static=yes --block-quic=no --set-http-proxy=yes
+    command: ppp --mode=client --tun-flash=yes --tun-static=yes --block-quic=no --set-http-proxy=yes
 # 定义网络
 networks:
   ${ppp_name}network:
@@ -304,8 +304,8 @@ create_or_modify_ppp_config() {
     # 检测公网出口/内网IP来提示用户
     curl -m 10 -s ip.sb
     ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1
-    
-    read -p "请输入VPS IP: " vps_ip
+    default_vps_ip="::"
+    read -p "请输入VPS IP地址（默认为${default_vps_ip}，服务端保持默认值即可）: " vps_ip
     read -p "请输入VPS 端口: " port
     # 设置监听Interface的默认值::用于ipv6
     default_lan_ip="::"

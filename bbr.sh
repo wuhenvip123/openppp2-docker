ae7943b41a6c714b40f894b627e070464aac71b8
rebecca554owen <rebecca554owen@gmail.com>
2024-06-12 22:37:37 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -108,62 +108,63 @@ write_sysctl_conf() {
     local qdisc=$2
     cat >> /etc/sysctl.conf << EOF
 # 系统文件描述符限制，设置最大文件描述符数量
-fs.file-max = $((1024 * 1024))  # 设置最大文件描述符数量
+fs.file-max = $((1024 * 1024))
 
 # 设置每个用户实例的 inotify 实例数量上限
-fs.inotify.max_user_instances = 8192  # 设置每个用户实例的 inotify 实例数量上限
+fs.inotify.max_user_instances = 8192
 
 # 网络核心配置
-net.core.rmem_max = $((64 * 1024 * 1024))  # 设置最大接收缓冲区大小
-net.core.wmem_max = $((64 * 1024 * 1024))  # 设置最大发送缓冲区大小
-net.core.netdev_max_backlog = 100000  # 设置接收队列的最大长度
-net.core.somaxconn = 1000000  # 设置最大连接数
-net.core.optmem_max = 65536  # 设置每个套接字的最大可选内存
+net.core.rmem_max = $((64 * 1024 * 1024))
+net.core.wmem_max = $((64 * 1024 * 1024))
+net.core.netdev_max_backlog = 100000
+net.core.somaxconn = 1000000
+net.core.optmem_max = 65536
 
 # TCP配置
-net.ipv4.tcp_syncookies = 0  # 禁用 syncookies 洪水攻击保护
-net.ipv4.tcp_tw_reuse = 1  # 允许复用 TIME-WAIT sockets 用于新的 TCP 连接
-net.ipv4.tcp_timestamps = 0  # 禁用 TCP 时间戳
-net.ipv4.tcp_rmem = 4096 87380 $((64 * 1024 * 1024))  # 设置 TCP 接收缓冲区
-net.ipv4.tcp_wmem = 4096 65536 $((64 * 1024 * 1024))  # 设置 TCP 发送缓冲区
-net.ipv4.tcp_fastopen = 3  # 开启 TCP Fast Open
-net.ipv4.tcp_window_scaling = 1  # 开启 TCP 窗口扩展
-net.ipv4.tcp_adv_win_scale = -2  # 设置 TCP 窗口缩放因子
-net.ipv4.tcp_mtu_probing = 1  # 开启 TCP MTU 探测
-net.ipv4.tcp_max_orphans = 262144  # 设置最大孤儿连接数
-net.ipv4.tcp_syn_retries = 3  # 设置 TCP SYN 重试次数
-net.ipv4.tcp_synack_retries = 3  # 设置 TCP SYN-ACK 重试次数
-net.ipv4.tcp_keepalive_time = 300  # 设置 TCP 保持连接时间
-net.ipv4.tcp_keepalive_probes = 2  # 设置 TCP 保持连接探测数
-net.ipv4.tcp_keepalive_intvl = 2  # 设置 TCP 保持连接探测间隔
-net.ipv4.tcp_fin_timeout = 10  # 设置 TCP 连接关闭超时时间
-net.ipv4.tcp_abort_on_overflow = 1  # 启用 TCP 溢出时的快速终止
-net.ipv4.tcp_max_syn_backlog = 8192  # 设置最大 SYN 等待队列长度
-net.ipv4.tcp_max_tw_buckets = 55000  # 设置最大 TIME-WAIT bucket 数量
+net.ipv4.tcp_syncookies = 0
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_rmem = 4096 87380 $((64 * 1024 * 1024))
+net.ipv4.tcp_wmem = 4096 65536 $((64 * 1024 * 1024))
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = -2
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_max_orphans = 262144
+net.ipv4.tcp_syn_retries = 3
+net.ipv4.tcp_synack_retries = 3
+net.ipv4.tcp_keepalive_time = 300
+net.ipv4.tcp_keepalive_probes = 2
+net.ipv4.tcp_keepalive_intvl = 2
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_abort_on_overflow = 1
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 55000
 
 # IP 转发配置
-net.ipv4.ip_forward = 1  # 启用 IP 转发
-net.ipv4.conf.all.forwarding = 1  # 启用所有接口的 IP 转发
-net.ipv4.conf.default.forwarding = 1  # 启用默认接口的 IP 转发
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.conf.default.forwarding = 1
 
 # IPv6 配置
-net.ipv6.conf.all.forwarding = 1  # 启用所有接口的 IPv6 转发
-net.ipv6.conf.default.forwarding = 1  # 启用默认接口的 IPv6 转发
-net.ipv6.conf.lo.forwarding = 1  # 启用本地接口的 IPv6 转发
-net.ipv6.conf.all.disable_ipv6 = 0  # 启用 IPv6
-net.ipv6.conf.default.disable_ipv6 = 0  # 启用默认接口的 IPv6
-net.ipv6.conf.lo.disable_ipv6 = 0  # 启用本地接口的 IPv6
-net.ipv6.conf.all.accept_ra = 2  # 接受 Router Advertisement
-net.ipv6.conf.default.accept_ra = 2  # 接受默认接口的 Router Advertisement
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.lo.forwarding = 1
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0
+net.ipv6.conf.all.accept_ra = 2
+net.ipv6.conf.default.accept_ra = 2
 
 # 调整虚拟内存行为
-vm.swappiness = 10  # 设置虚拟内存交换使用率
-vm.overcommit_memory = 1  # 允许内存过度分配
+vm.swappiness = 10
+vm.overcommit_memory = 1
 
 # 设置 TCP 拥塞控制算法
-net.ipv4.tcp_congestion_control = $congestion_control  # 设置 TCP 拥塞控制算法
+net.ipv4.tcp_congestion_control = $congestion_control
+
 # 设置默认队列规则
-net.core.default_qdisc = $qdisc  # 设置默认队列规则
+net.core.default_qdisc = $qdisc
 EOF
 }
 

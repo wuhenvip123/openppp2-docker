e6ca840a67a5925d9d0721ae2cc371f453519029
rebecca554owen <rebecca554owen@gmail.com>
2024-06-12 13:46:23 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 # 检查是否安装了Docker
 if ! command -v docker &> /dev/null
 then
-    echo "Docker未安装，是否安装Docker？( yes / no ) 回车默认安装。"
+    echo "Docker未安装，是否安装Docker？回车默认安装，请输入 yes 或 no "
     read install_docker
     if [[ "$install_docker" == "NO" || "$install_docker" == "no" || "$install_docker" == "N" || "$install_docker" == "n" ]]; then
         echo "未安装Docker，脚本退出。"
@@ -14,6 +14,7 @@ then
         sh get-docker.sh
         if [ $? -ne 0 ]; then
             echo "Docker安装失败，请检查网络连接或手动安装。"
+            echo "curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun"
             exit 1
         fi
         echo "Docker安装成功。"
@@ -22,6 +23,27 @@ fi
 
 selected_container=""
 containers=()
+
+# 列出所有容器并选择容器
+select_container() {
+    containers=($(docker ps -a --format "{{.ID}} {{.Names}}"))
+    if [ ${#containers[@]} -eq 0 ]; then
+        echo "没有找到容器。"
+    else
+        echo "所有容器："
+        for i in "${!containers[@]}"; do
+            echo "$((i/2+1)). ID: ${containers[i]} 名称: ${containers[i+1]}"
+            ((i++))
+        done
+        read -p "输入要选择的容器序号：" container_index
+        if [ $container_index -gt 0 ] && [ $container_index -le $(( ${#containers[@]} / 2 )) ]; then
+            selected_container=${containers[((container_index-1)*2)]}
+            echo "选中的容器: $selected_container"
+        else
+            echo "无效的序号。"
+        fi
+    fi
+}
 
 # 显示操作菜单
 while true; do
@@ -32,63 +54,44 @@ while true; do
     fi
 
     echo "请选择操作："
-    echo "1. 列出所有容器"
-    echo "2. 选择容器"
-    echo "3. 启动容器"
-    echo "4. 停止容器"
-    echo "5. 重启容器"
-    echo "6. 更新容器镜像"
-    echo "7. 清空无用的镜像"
-    echo "8. 退出"
+    echo "1. 列出并选择容器"
+    echo "2. 启动容器"
+    echo "3. 停止容器"
+    echo "4. 重启容器"
+    echo "5. 更新容器镜像"
+    echo "6. 清空无用的镜像"
+    echo "7. 退出"
     read -p "输入数字选择操作：" choice
 
     case $choice in
         1)
-            containers=($(docker ps -a --format "{{.ID}} {{.Names}}"))
-            if [ ${#containers[@]} -eq 0 ]; then
-                echo "没有找到容器。"
-            else
-                echo "所有容器："
-                for i in "${!containers[@]}"; do
-                    echo "$((i/2+1)). ID: ${containers[i]} 名称: ${containers[i+1]}"
-                    ((i++))
-                done
-            fi
+            select_container
             ;;
         2)
-            if [ ${#containers[@]} -eq 0 ]; then
-                echo "请先列出容器。"
-            else
-                read -p "输入要选择的容器序号：" container_index
-                if [ $container_index -gt 0 ] && [ $container_index -le $(( ${#containers[@]} / 2 )) ]; then
-                    selected_container=${containers[((container_index-1)*2)]}
-                else
-                    echo "无效的序号。"
-                fi
-            fi
-            ;;
-        3)
             if [ -n "$selected_container" ]; then
                 docker start $selected_container
             else
                 echo "请先选择一个容器。"
+                select_container
             fi
             ;;
-        4)
+        3)
             if [ -n "$selected_container" ]; then
                 docker stop $selected_container
             else
                 echo "请先选择一个容器。"
+                select_container
             fi
             ;;
-        5)
+        4)
             if [ -n "$selected_container" ]; then
                 docker restart $selected_container
             else
                 echo "请先选择一个容器。"
+                select_container
             fi
             ;;
-        6)
+        5)
             if [ -n "$selected_container" ]; then
                 read -p "输入要更新的容器镜像名称：" image_name
                 docker pull $image_name
@@ -98,12 +101,13 @@ while true; do
                 docker run -d --name $selected_container $image_name
             else
                 echo "请先选择一个容器。"
+                select_container
             fi
             ;;
-        7)
+        6)
             docker image prune -f
             ;;
-        8)
+        7)
             echo "退出脚本。"
             exit 0
             ;;
